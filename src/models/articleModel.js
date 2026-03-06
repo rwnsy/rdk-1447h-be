b28efc9efe49ed4e59af9a63153ca31cc7264078
fahmiirfanfaiz <fahmiirfanfaiz01@gmail.com>
2026-03-06 11:46:44 +0700

    feat: add image upload functionality for articles and galleries

--- src/models/articleModel.js
+++ src/models/articleModel.js
@@ -1,13 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const imageSchema = new Schema(
+  {
+    originalName: { type: String, required: true },
+    mimeType: { type: String, required: true },
+    fileExtension: { type: String, required: true },
+    fileSize: { type: Number, required: true },
+    base64Data: { type: String, required: true },
+    fileHash: { type: String, required: true },
+  },
+  { _id: false },
+);
+
 const articleSchema = new Schema(
   {
     title: { type: String, required: true },
     content: { type: String, required: true },
+    summary: { type: String, required: true },
+    status: { type: String, enum: ["Draft", "Published"], default: "Draft" },
     category: { type: String, required: true },
     publishedDate: { type: Date, required: true },
     writer: { type: String, required: true },
+    image: { type: imageSchema, default: null },
   },
   { timestamps: true },
 );
